b5e71db0f42ba3bbc3dce1579a18e26cefa39dbc
Dargonrol <dargonrol@gmail.com>
2024-06-20 21:03:24 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/TouhouProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TouhouProject.dir/main.cpp.obj"
+  "CMakeFiles/TouhouProject.dir/main.cpp.obj.d"
+  "TouhouProject.exe"
+  "TouhouProject.exe.manifest"
+  "TouhouProject.pdb"
+  "libTouhouProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TouhouProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TouhouProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TouhouProject.
+# This may be replaced when dependencies are built.
